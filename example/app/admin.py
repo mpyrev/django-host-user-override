e9c7a9f73d818bccc9a60278f4f3ddd325c31f51
Mikhail Pyrev <m.pyryev@dev.kokoc.com>
2018-02-09 12:38:44 +0300

    Python 2 compat fix

--- example/app/admin.py
+++ example/app/admin.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-from __future__ import unicode_literals
+from __future__ import unicode_literals, absolute_import
 
 from django.contrib import admin
 from django.contrib.auth import get_user_model
