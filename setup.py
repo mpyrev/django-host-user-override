84679c9a7ec30f55148a5f665f6b09adac2cd6cc
Mikhail Pyrev <m.pyryev@dev.kokoc.com>
2018-04-12 15:07:31 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-host-user-override',
-    version='0.2.1',
+    version='0.3.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=REQUIREMENTS,
